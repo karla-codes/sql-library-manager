68021dc5dce480c8fce1934d113b03b3d3a12bd2
karla-walker <indiablonix@gmail.com>
2021-08-03 03:03:00 -0700

    Added asyncHandler function and set up route to display all books

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
+
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  };
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    res.render('index', { books, title: 'Books' });
+
+    // console.log(books);
+  })
+);
 
 module.exports = router;
