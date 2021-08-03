21edd68252fd720be0b5549c850d955f71f8dfa4
karla-walker <indiablonix@gmail.com>
2021-08-03 03:00:42 -0700

    Added async function that syncs book model with database

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const { sequelize } = require('./models/index');
+
+// syncs model with databse
+(async () => {
+  try {
+    await sequelize.sync();
+    console.log('Connection has been established successfully');
+  } catch (err) {
+    console.error('Unable to connect to database:', err);
+  }
+})();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,12 +35,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
