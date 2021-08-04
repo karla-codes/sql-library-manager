2bd19409fbabdc73831d806a7a9e07d20c888577
karla-walker <indiablonix@gmail.com>
2021-08-04 02:07:15 -0700

    Added home page router

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,9 @@
-var express = require('express');
-var router = express.Router();
-const { Book } = require('../models');
+const express = require('express');
+const router = express.Router();
 
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch (err) {
-      res.status(500).send(err);
-    }
-  };
-}
-
-/* GET home page. */
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const books = await Book.findAll();
-    res.render('index', { books, title: 'Books' });
-
-    // console.log(books);
-  })
-);
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.redirect('/books');
+});
 
 module.exports = router;
