b90935fa152bb7d423b0ace3e81f4a91f2d1f11e
karla-walker <indiablonix@gmail.com>
2021-08-04 02:29:41 -0700

    Added the POST new book router and GET individual book router

--- routes/books.js
+++ routes/books.js
@@ -28,8 +28,22 @@ router.get('/new', (req, res) => {
 });
 
 // POST new book
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const book = await Book.create(req.body);
+    res.redirect('/books/' + book.id);
+  })
+);
 
 // GET individual book info
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    res.redirect('/');
+  })
+);
 
 // POST updated book info
 
